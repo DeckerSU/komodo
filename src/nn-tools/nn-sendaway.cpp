31efaf9523d58589186453d2dcad8d74695be1c6
DeckerSU <support@decker.su>
2020-05-10 04:40:56 +0300

    add template for build nn-tools

---
+++ src/nn-tools/nn-sendaway.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2013 The Bitcoin Core developers
+// Copyright (c) 2020 Decker
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/******************************************************************************
+ * Copyright © 2014-2020 The SuperNET Developers.                             *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * SuperNET software, including this file may be copied, modified, propagated *
+ * or distributed except according to the terms contained in the LICENSE file *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+// Use CONFIGURE_FLAGS=--enable-nntools zcutil/build.sh -j$(nproc) to build with these tools.
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}

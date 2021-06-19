3887d2335861c0a20ad51a03b80ee42e86069bfe
DeckerSU <deckersu@protonmail.com>
2021-06-19 18:08:39 +0300

    change stratum.cpp version, add MPL-2.0 source

--- src/stratum.cpp
+++ src/stratum.cpp
@@ -3,12 +3,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// Copyright (c) 2021-2021 Decker
+
 /**
  * @file stratum.cpp
  * @author Decker
  * @brief Equihash Stratum implementation for Komodo (KMD) daemon
- * @version 0.1.2
- * @date 2021-06-16
+ * @version 0.1.3
+ * @date 2021-06-19
  *
  * @copyright Copyright (c) 2021
  *
